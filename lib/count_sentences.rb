cb1f879fe3a3ab693391511fd1ee8dc90718851a
Yuyi <yuyi365@gmail.com>
2021-11-22 13:54:00 -0500

    commit tree ec23d56a89e23efb2ea648cfdf95baff84351270

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split_sentence = self.split(/\.|\?|!/)
+    split_sentence.filter {|sentence| !sentence.empty? }.size
   end
 end
